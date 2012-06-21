4981d4eff334da6c63bd26c6e506944ce690c162
pdclark <pdclark@pdclark.com>
2012-06-20 16:38:21 -0800

    Prevent recursion and modifying existing sprites

---
+++ monochrome-admin-icons.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Monochrome Admin Icons
+Description: Make third-party admin icons behave like WordPress default icons: Monochrome when inactive, color when active or hovered.
+Version: 1.0
+Author: Brainstorm Media
+Author URI: http://brainstormmedia.com
+*/
+
+add_action('admin_init', 'storm_monochrome_admin_icons');
+
+function storm_monochrome_admin_icons(){
+	wp_enqueue_script('monochrome-admin-icons', plugin_dir_url( __FILE__ ).'monochrome.js', array('jquery'), '1.0', true);
+}
--- monochrome.js
+++ monochrome.js
@@ -1,31 +1,44 @@
 jQuery(document).ready(function($){
-	var $ = jQuery;
-
 	$('#adminmenu > li:not(.wp-has-current-submenu)').hover(
 		function(){
-			$(this).find('img').eq(0).css('opacity', 1);
+			$(this).find('img.mono').eq(0).css('opacity', 1);
 		},
 		function(){
-			$(this).find('img').eq(0).css('opacity', 0);
+			$(this).find('img.mono').eq(0).css('opacity', 0);
 		}
 	);
 	
 	$('#adminmenu img').hide(); // Avoid image flicker
-
 });
 
 // Attach to load event of individual images in the Admin Menu
-// Technique from http://webdesignerwall.com/tutorials/html5-grayscale-image-hover
-jQuery('#adminmenu > li:not(.wp-has-current-submenu) img').load(function($){
-	var $ = jQuery;
+// This selector doesn't effect default WordPress icons, because they're background sprites, not img tags
+jQuery('#adminmenu > li:not(.wp-has-current-submenu) img').bind('load', storm_monochrome_imgs);
 
-	var el = $(this);
-		el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"998","opacity":"0"}).insertBefore(el).queue(function(){
-			var el = $(this);
-			el.parent().css({"width":this.width,"height":this.height});
-			el.dequeue();
+function storm_monochrome_imgs(){
+	// Technique from http://webdesignerwall.com/tutorials/html5-grayscale-image-hover
+	var $ = jQuery;
+	var img = $(this);
+	console.log( img.height() );
+
+	if ( img.parent().hasClass('mono-wrap') ) { return; } // Prevent recursion
+	if ( img.height() > 20 ) { return; } // Single icons are 16px tall. This is probably a sprite already.
+
+	console.log( $(this).parent().attr('aria-label')+': '+img.attr('src'));
+
+	img.css({'position':'absolute'})
+		.wrap('<div class="mono-wrap" style="display: inline-block">')
+		.clone()
+		.addClass('mono')
+		.css({'position':'absolute','z-index':'998','opacity':'0','display':'block'})
+		.insertBefore(img)
+		.queue(function(){
+			var img = $(this);
+			img.parent().css({'width':this.width,'height':this.height});
+			img.dequeue();
 		});
-		this.src = grayscale(this.src);
+
+	this.src = grayscale(this.src);
 	
 	// Grayscale w canvas method
 	function grayscale(src){
@@ -49,7 +62,7 @@ jQuery('#adminmenu > li:not(.wp-has-current-submenu) img').load(function($){
 		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
 		return canvas.toDataURL();
 	 }
-});
+}
 
 // All images on the page have loaded.
 // It's safe to show Admin menu images
